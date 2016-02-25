ee33c39084274055940dffe76d23b3e5ea7e6389
jagi <luke.jagodzinski@gmail.com>
2016-02-25 21:05:16 +0100

    Make class compatible with SimpleSchema & Astronomy

--- validation-error.js
+++ validation-error.js
@@ -1,21 +1,29 @@
-/* global ValidationError:true */
-
-// This is exactly what comes out of SS.
-const errorPattern = [{
+// The "details" property of the ValidationError must be an array of objects
+// containing at least two properties. The "name" and "type" properties are
+// required.
+const errorsPattern = [Match.ObjectIncluding({
   name: String,
-  type: String,
-  details: Match.Optional(Object)
-}];
+  type: String
+})];
 
-ValidationError = class extends Meteor.Error {
-  constructor(errors, message = 'Validation Failed') {
-    check(errors, errorPattern);
-    check(message, String);
+class ValidationError extends Meteor.Error {
+  constructor(errors, reason = ValidationError.DEFAULT_REASON) {
+    check(errors, errorsPattern);
+    check(reason, String);
 
-    super(ValidationError.ERROR_CODE, message, errors);
+    return super(ValidationError.ERROR_CODE, reason, errors);
   }
+
+  // Static method checking if a given Meteor.Error is an instance of
+  // ValidationError.
+  static is(err) {
+    return err &&
+      err instanceof Meteor.Error &&
+      err.error === ValidationError.ERROR_CODE;
+  };
 };
 
-// If people use this to check for the error code, we can change it
-// in future versions
+// Universal validation error code to be use in applications and packages.
 ValidationError.ERROR_CODE = 'validation-error';
+// Default validation error message that can be changed globally.
+ValidationError.DEFAULT_REASON = 'Validation failed';
